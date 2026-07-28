acb5d59fbc9ebe554853daa0e679866bdfbb6e4c
Janya <janyakumarappas@gmail.com>
2026-07-28 17:52:41 +0530

    Ready for Render deployment

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,7 @@ import bookingRoutes from "./routes/booking.js";
 import courseRoutes from "./routes/course.js";
 import enrollmentRoutes from "./routes/enrollment.js";
 import galleryRoutes from "./routes/gallery.js";
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
@@ -36,8 +37,8 @@ app.get("/", (req, res) => {
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected");
-    app.listen(5000, () => {
-      console.log("Server running on port 5000");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((error) => {
